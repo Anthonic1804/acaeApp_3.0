eedd52a7551d12f6899229a662fa560e82b9c4c4
Anthonic1804 <adol.antonio1990@gmail.com>
2023-04-20 11:57:18 -0600

    LOGIN TRABAJANDO AL 100% CON LA VERIFICACION DE DISPOSITIVOS

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -24,51 +24,6 @@ namespace servicio.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Login credenciales)
         {
-            //if (credenciales != null)
-            //{
-
-            //    var validacion = context.empleados.Where(e => e.Usuario_App.Trim().ToUpper() == credenciales.Usuario && e.Clave_App == credenciales.Clave).First();
-            //    if (validacion != null)
-            //    {
-            //        var fecha_actual = DateTime.Now;
-            //        var respuestaLogin = "";
-
-            //        //COMPROBACION DE MOVIL SOLO PARA DISPOSITIVOS AUTORIZADOS POR LA EMPRESA
-            //        if (validacion.Identidad_App == null || validacion.Identidad_App == credenciales.Identidad)
-            //        {
-            //            validacion.Identidad_App = credenciales.Identidad;
-            //            validacion.Estado_App = "ACTIVO";
-            //            validacion.Ultima_Conexion_App = fecha_actual;
-            //            context.SaveChanges();
-            //            respuestaLogin = "Credenciales validas";
-
-            //            return StatusCode(StatusCodes.Status200OK,
-            //            new RespuestaLogin { Error = validacion.Id, nombreEmpleado = validacion.Empleado, Response = respuestaLogin, Identidad = validacion.Identidad_App, GeneraToken = validacion.Generar_Token, Estado = validacion.Estado_App });
-            //        }
-            //        else {
-            //            respuestaLogin = "Dispositivo no Autorizado";
-            //            validacion.Estado_App = "INACTIVO";
-            //            context.SaveChanges();
-
-            //            return StatusCode(StatusCodes.Status204NoContent,
-            //            new RespuestaLogin { Error = 500, nombreEmpleado = "", Response = respuestaLogin, Identidad = "", Estado = "" });
-            //        }
-
-
-            //    }
-            //    else
-            //    {
-            //        return StatusCode(StatusCodes.Status200OK,
-            //           new RespuestaLogin { Error = 0, nombreEmpleado = "", Response = "Clave o Usuario Invalidos", Identidad = "", Estado = "" });
-            //    }
-
-            //}
-            //else
-            //{
-            //    return StatusCode(StatusCodes.Status204NoContent,
-            //            new RespuestaLogin { Error = 500, nombreEmpleado = "", Response = "No se han Recibido Parametros", Identidad = "", Estado = "" });
-            //}
-
             if (credenciales == null || string.IsNullOrEmpty(credenciales.Usuario) || string.IsNullOrEmpty(credenciales.Clave))
             {
                 return BadRequest(new RespuestaLogin { Error = 0, nombreEmpleado = "", Response = "No se han recibido parámetros", Identidad = "", Estado = "" });
@@ -111,77 +66,7 @@ namespace servicio.Controllers
             }
         } //login del usuario 
 
-        //[HttpPost("estado")]
-        //public IActionResult PostEstado([FromBody] Login credenciales)
-        //{
-        //    if (credenciales != null)
-        //    {
-
-        //        // VERIFICAR SI SE SOBREPASA EL NUMERO MAXIMO DE SESIONES PERMITIDAS
-        //        var config = context.config.First();
-        //        var sesiones_activas = context.empleados.Count(x => x.Estado_App.Trim() == "ACTIVO");
-        //        var respuestaLogin = "";
-
-        //        var validacion = context.empleados.Where(e => e.Usuario_App.Trim().ToUpper() == credenciales.Usuario && e.Clave_App == credenciales.Clave).FirstOrDefault();
-
-        //        if (validacion.Identidad_App != null && validacion.Identidad_App != credenciales.Identidad)
-        //        {
-        //            validacion.Estado_App = "INACTIVO";
-        //            context.SaveChanges();
-
-        //            respuestaLogin = "Dispositivo no autorizado";
-
-        //            return StatusCode(StatusCodes.Status200OK,
-        //               new RespuestaLogin { Error = 0, nombreEmpleado = "", Response = respuestaLogin, Identidad = "", Estado = "" });
-        //        }
-
-        //        if (validacion != null)
-        //        {
-        //            var estado = validacion.Estado_App == null ? "" : validacion.Estado_App.Trim();
-
-        //            if (estado != "ACTIVO")
-        //            {
-        //                sesiones_activas++;
-        //            }
-        //        }
-        //        else
-        //        {
-
-        //            return StatusCode(StatusCodes.Status200OK,
-        //               new RespuestaLogin { Error = 0, nombreEmpleado="", Response = "Clave o Usuario Invalidos", Identidad = "", Estado = "" });
-
-        //            sesiones_activas++;
-        //        }
-
-
-        //        if (sesiones_activas > config.App_max_licencias)
-        //        {
-        //            return StatusCode(StatusCodes.Status200OK,
-        //               new RespuestaLogin { Error = 0, nombreEmpleado="", Response = "Se ha llegado al maximo de sesiones activas", Identidad = "", Estado = "" });
-        //        }
-        //        else
-        //        {
-        //            if (validacion != null)
-        //            {
-        //                var Identidad_env = validacion.Identidad_App == null ? "" : validacion.Identidad_App;
-        //                var Estado_env = validacion.Estado_App == null ? "" : validacion.Estado_App;
-        //                return StatusCode(StatusCodes.Status200OK,
-        //                    new RespuestaLogin { Error = validacion.Id, nombreEmpleado = validacion.Empleado, Response = "Credenciales validas", Identidad = Identidad_env, GeneraToken = validacion.Generar_Token, Estado = Estado_env });
-        //            }
-        //            else
-        //            {
-        //                return StatusCode(StatusCodes.Status200OK,
-        //                   new RespuestaLogin { Error = 0, nombreEmpleado = "", Response = "Clave o Usuario Invalidos", Identidad = "", Estado = "" });
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return StatusCode(StatusCodes.Status204NoContent,
-        //                new RespuestaLogin { Error = 500, nombreEmpleado = "", Response = "No se han Recibido Parametros", Identidad = "", Estado = "" });
-        //    }
-        //} // ver estado de login del usuario 
-
+        
         [HttpPost("estado")]
         public async Task<IActionResult> PostEstado([FromBody] Login credenciales)
         {
